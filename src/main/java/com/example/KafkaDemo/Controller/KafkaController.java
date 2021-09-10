5e4676c029129c9dc0ff66e888b022dc4900f9b6
manikanthabolisetty <manikantha.b@thoughtworks.com>
2021-09-10 12:04:30 +0530

    Initial commit

---
+++ src/main/java/com/example/KafkaDemo/Controller/KafkaController.java
@@ -0,0 +1,24 @@
+package com.example.KafkaDemo.Controller;
+
+import com.example.KafkaDemo.Producer.ProducerApi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.websocket.server.PathParam;
+
+@RestController
+public class KafkaController {
+
+    @Autowired
+    ProducerApi producerApi;
+
+    @GetMapping("/publish")
+    public ResponseEntity<String> publishMessage(@RequestParam("message") String message){
+        producerApi.sendMessage(message);
+        return ResponseEntity.ok("Successfully send the message to topic");
+    }
+}
---
+++ src/main/java/com/example/KafkaDemo/Producer/ProducerApi.java
@@ -0,0 +1,31 @@
+package com.example.KafkaDemo.Producer;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.stereotype.Service;
+
+import java.util.Properties;
+@Service
+public class ProducerApi {
+
+    public void sendMessage(String message){
+
+        Properties properties= new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+
+        KafkaProducer<String,String> firstProducer = new KafkaProducer<String, String>(properties);
+
+        ProducerRecord<String,String> producerRecord= new ProducerRecord<String,String>("kafkaTesting", "This is my first message");
+
+        firstProducer.send(producerRecord);
+        firstProducer.flush();
+        firstProducer.close();
+
+    }
+
+
+}
